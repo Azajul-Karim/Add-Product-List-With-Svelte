1dee596e8d559d157963d1c351cef2d78e492df2
Azajul Karim <azajulkarim2021@gmail.com>
2022-08-27 12:07:33 +0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import AddListProduct from "./components/AddListProduct.svelte";
+  import ListProduct from "./components/ListProduct.svelte";
+
+  let products = [];
+  function addNewProduct(event) {
+    let { product } = event.detail;
+    products = [...products, product];
+  }
+</script>
+
+<div class="container mt-4">
+  <AddListProduct on:add_product={addNewProduct} />
+  <ListProduct {products} />
+</div>
---
+++ src/components/AddListProduct.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+  let fire = createEventDispatcher();
+  export let product = {
+    productName: "",
+    price: "",
+  };
+
+  function addProduct(event) {
+    event.preventDefault();
+    fire("add_product", { product });
+
+    product = {
+      productName: "",
+      price: "",
+    };
+  }
+</script>
+
+<form on:submit={(event) => addProduct(event)}>
+  <label for="title"><strong>PRODUCT</strong></label>
+  <div class="input-group mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text">
+        <i class="bi bi-cart-fill" />
+      </span>
+    </div>
+    <input
+      type="text"
+      class="form-control"
+      aria-label="Amount (to the nearest dollar)"
+      bind:value={product.productName}
+    />
+  </div>
+  <label for="title"><strong>PRICE</strong></label>
+  <div class="input-group mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text"><i class="bi bi-currency-dollar" /> </span>
+    </div>
+    <input
+      type="number"
+      class="form-control"
+      aria-label="Amount (to the nearest dollar)"
+      bind:value={product.price}
+    />
+  </div>
+  <br />
+  <input type="submit" value="ADD" class="btn btn-dark btn-block" />
+  <br />
+</form>
